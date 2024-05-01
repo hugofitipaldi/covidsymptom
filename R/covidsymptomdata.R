73dbbf7bfe63f39427f12562f58c56d1be868d77
Hugo Fitipaldi <33068886+hugofitipaldi@users.noreply.github.com>
2024-05-01 20:56:23 +0200

    fixing symptoms dataset

--- R/covidsymptomdata.R
+++ R/covidsymptomdata.R
@@ -14,9 +14,27 @@ postcode_estimates$Datum <- as.Date(postcode_estimates$Datum, format = "%Y-%m-%d
 postcode_estimates$Postnummer <- as.character(postcode_estimates$Postnummer)
 county_estimates$Lan <- stringi::stri_trans_general(str = county_estimates$Lan,id = "Latin-ASCII")
 postcode_estimates$Ort <- stringi::stri_trans_general(str = postcode_estimates$Ort,id = "Latin-ASCII")
+symptoms$Symptom <- stringi::stri_trans_general(str = symptoms$Symptom,id = "Latin-ASCII")
 csss_tests$date <- as.Date(csss_tests$date, format = "%Y-%m-%d")
 csss_tests$test_type <- "Snabbtester"
-symptoms$Datum <- as.Date(symptoms$Datum, format = "%Y-%m-%d")
+
+# Define the translation vector
+translations <- c(
+  Aptitloshet = "Loss of Appetite",
+  Diarre = "Diarrhea",
+  Feber = "Fever",
+  `Forlorat eller forandrat lukt-/smaksinne` = "Lost or changed sense of smell or taste",
+  `Frossa/huttrar` = "Chills or Shivering",
+  Heshet = "Hoarseness",
+  `Ihallande hosta` = "Persistent Cough",
+  `Ont eller irritation i ogon` = "Eye pain or irritation",
+  `Ont i halsen` = "Sore Throat",
+  `Ovanlig muskelsmarta` = "Unusual muscle pain",
+  `Svar trotthet` = "Severe fatigue"
+)
+
+# Apply the translations to the 'Symptom' column
+symptoms$Symptom <- translations[symptoms$Symptom]
 
 # Data update
 usethis::use_data(national_estimates, county_estimates, postcode_estimates, csss_tests, symptoms,  overwrite = TRUE)
